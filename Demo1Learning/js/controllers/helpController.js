247f452f95f22a45daa0c78cd179f5866b7c4f48
Jitender Singh <singh.90.jitendra@gmail.com>
2014-10-27 16:38:38 +0530

    ng-route sub view completed

--- Demo1Learning/js/controllers/helpController.js
+++ Demo1Learning/js/controllers/helpController.js
@@ -3,6 +3,11 @@ define(['controllers'],function(controllers){
 	var helpController = controllers.controller('helpController',function($scope,$route,$routeParams ,$location){
 		  $scope.message = 'This is helpController screen';
 		  $scope.listtype = $routeParams.param;
+		  $scope.$watch('listtype',function(val){
+		 		$location.path('help/'+val);
+		});
 	});	
+
  	return helpController;
+
 });
